4e5bbb5e53af00b833d7d973f04506493229a166
Max Stouten <max@liftov.be>
2020-09-02 09:00:22 +0200

    react practice course

--- src/index.js
+++ src/index.js
@@ -1,17 +1,124 @@
-import React from 'react';
+import React, {useEffect, useState, useReducer} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const lakesList = [
+  {id: "1", name:"Echo", trailhead:"something"},
+  {id: "2", name:"tahoe", trailhead: "wrights"},
+  {id: "3", name:"wamp", trailhead: "bayview"},
+]
+
+
+
+function Open() {
+  const [status, setStatus] = useState("open");
+  return (
+    <div>
+      <h1>Status: {status} </h1>
+      <button onClick={() => setStatus("Closed")} >Closed</button>
+      <button onClick={() => setStatus("Open")}> Open</button>
+      <button onClick={() => setStatus("Back in five")}>Break</button>
+    </div>
+  )
+}
+
+function Checked() {
+  const [checked, toggle] = useReducer(
+    checked => !checked,
+    false
+  );
+  return  (
+    <div>
+      <input type="checkbox" value={checked} onChange={toggle}/>
+      {checked ? "checked" : "not checked"}
+    </div>
+  )
+}
+
+function GithubUser({ login }) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then(res => res.json())
+      .then(setData)
+      .catch(console.error)
+  }, [])
+  if (data) {
+    return  <div>
+      <h1>
+        {data.login}
+      </h1>
+      <img src={data.avatar_url} width={100} alt=""/>
+      <p>
+        {data.company}
+      </p>
+    </div>
+  }
+  return null;
+}
+
+function Logging() {
+  const [val, setVal] = useState("");
+  const [val2, setVal2] = useState("");
+  useEffect(() => {
+    console.log(`field 1 ${val}`)
+  }, [val])
+  useEffect(() => {
+    console.log(`field 2 ${val2}`)
+  }, [val2])
+  return (
+    <div>
+      <label >
+             Fav phrase
+        <input type="text"
+               value={val}
+               onChange={e => setVal(e.target.value)}
+        />
+      </label>
+      <br/>
+      <label>
+             SEcond fav phraz
+        <input type="text"      value={val2}
+               onChange={e => setVal2(e.target.value)}/>
+
+      </label>
+    </div>
+  )
+}
+
+
+function Lake({name}) {
+  return (
+    <div>
+      <h1>Visit {name}</h1>
+    </div>
+  )
+}
+
+function SkiResrt({name}) {
+  return  (
+    <div>
+      <h2>Go skiing! {name}</h2>
+    </div>
+  )
+}
+
+//Conditional rendering
+function App() {
+ return <GithubUser login="KapiteinKattenkwaad" />
+}
+
+// function App(props) {
+//   if (props.season === 'summer') {
+//      return <Lake name="Jenny" />
+//   } else if (props.season === 'winter') {
+//     return <SkiResrt name="skirestor" />
+//   }
+// }
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
